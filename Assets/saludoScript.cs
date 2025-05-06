7471d63cc52fb0abd5ee10ca6c5d903125453bcb
Jeremiasbercellini <167231647+Jeremiasbercellini@users.noreply.github.com>
2025-05-06 12:14:57 -0300

    Terminado

---
+++ Assets/saludoScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class saludoScript : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+    string nombreUsuario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = "";
+    }
+    public void saludar()
+    {
+        txtSaludo.text = "Hola " + inputNombre.text;
+    }
+}
