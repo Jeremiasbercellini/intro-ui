7ab414cec44ca4b3ce9dd97e37772e8be13dad69
Jeremiasbercellini <167231647+Jeremiasbercellini@users.noreply.github.com>
2025-05-09 09:07:00 -0300

    introUI empezado

---
+++ Assets/sumaUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class sumaUI : MonoBehaviour
+{
+    public TextMeshProUGUI txtResultado;
+    public TMP_InputField inputNum1;
+    public TMP_InputField inputNum2;
+    int num1;
+    int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = "";
+
+    }
+    public void sumarYmostrar()
+    {
+        num1 = int.Parse(inputNum1.text);
+        num2 = int.Parse(inputNum2.text);
+        txtResultado.text = (num1 + num2).ToString();
+    }
+}
